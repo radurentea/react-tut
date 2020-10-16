e515e368679f0f775c20aa6002694135104b00fa
Radu Rentea <radu.rentea@garrettmotion.com>
2020-10-16 06:30:35 +0300

    Updates

---
+++ precache-manifest.c572eb2fad24c7be64f979699d237148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ece1f07e3cf0e9e4eebc902639bbb5",
+    "url": "/react-tut/index.html"
+  },
+  {
+    "revision": "3afaedc88045cac64a84",
+    "url": "/react-tut/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "03cbd7b31d2ee5de4594",
+    "url": "/react-tut/static/css/main.a2311fff.chunk.css"
+  },
+  {
+    "revision": "3afaedc88045cac64a84",
+    "url": "/react-tut/static/js/2.fa3c44ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tut/static/js/2.fa3c44ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03cbd7b31d2ee5de4594",
+    "url": "/react-tut/static/js/main.9f0352b8.chunk.js"
+  },
+  {
+    "revision": "079cf540202bf3cccc4d",
+    "url": "/react-tut/static/js/runtime-main.b464e887.js"
+  }
+]);
